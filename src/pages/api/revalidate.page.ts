a5b6ed3042b0913dc2aa4dc0ffb3b598b86f3ad7
Patrick Huskey <phuskey@us.ci.org>
2024-02-02 21:08:25 -0600

    Change revalidate endpoint to use revalidatePath

--- src/pages/api/revalidate.page.ts
+++ src/pages/api/revalidate.page.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { revalidatePath } from 'next/cache';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const url = req.body.url;
 
   try {
     // Revalidate the URLs for those documents
-    await res.revalidate(url);
+    revalidatePath(url);
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue to show
